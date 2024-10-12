aff9a519bdf364ae0d37b4acc9bb367aa6fdb48f
Alaa <masalha.alaa@gmail.com>
2024-10-12 17:22:16 +0300

    Added missing translation files

---
+++ gui/translations/word_detailed_display_en.ts
@@ -0,0 +1,12 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="ar_EG">
+<context>
+    <name>Dialog</name>
+    <message>
+        <location filename="../word_detailed_display.ui" line="14"/>
+        <source>Dialog</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+</TS>
