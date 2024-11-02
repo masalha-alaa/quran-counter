304e2ea8093b4002005ae54d5c1185062c80558b
Alaa <masalha.alaa@gmail.com>
2024-11-02 23:00:51 +0200

    Pyinstaller one dir instead of one file (faster startup)

--- convert_to_exe.bat
+++ convert_to_exe.bat
@@ -1,5 +1,5 @@
 CALL .venv/Scripts/activate
 
-CALL pyinstaller --name=QuranCounter --onefile --windowed --add-data "data;data" --add-data "gui/resources; gui/resources" --add-data "translations;translations" --add-data "fonts;fonts" --add-data "config.yml;." --add-data "surah_index.yml;." main.py
+CALL pyinstaller --name=QuranCounter --onedir --windowed --add-data "data;data" --add-data "gui/resources;gui/resources" --add-data "translations;translations" --add-data "fonts;fonts" --add-data "config.yml;." --add-data "surah_index.yml;." main.py
 
 CALL deactivate
