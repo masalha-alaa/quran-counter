099366dff8e490b5926ce28933ec5681c4b6e9e6
Alaa <masalha.alaa@gmail.com>
2024-11-27 22:51:58 +0200

    Updated convert_to_exe.bat

--- convert_to_exe.bat
+++ convert_to_exe.bat
@@ -1,5 +1,8 @@
-CALL .venv/Scripts/activate
+python -m venv temp_venv
+CALL temp_venv/Scripts/activate
+CALL pip install -r minimal_requirements.txt
 
-CALL pyinstaller --name=QuranCounter --onedir --windowed --add-data "data;data" --add-data "gui/resources;gui/resources" --add-data "translations;translations" --add-data "fonts;fonts" --add-data "config.yml;." --add-data "surah_index.yml;." main.py
+CALL pyinstaller --name=QuranCounter --onedir --windowed --add-data "data;data" --add-data "gui/resources;gui/resources" --add-data "translations;translations" --add-data "fonts;fonts" --add-data "surah_index.yml;." main.py
 
 CALL deactivate
+RD /s /q "temp_venv"
