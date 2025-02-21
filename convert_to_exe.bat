f1576f3082c9aeccfd7b599c7b80419ec59f9fa8
Alaa <masalha.alaa@gmail.com>
2025-02-21 22:29:22 +0200

    Update convert_to_exe.bat

--- convert_to_exe.bat
+++ convert_to_exe.bat
@@ -1,5 +1,5 @@
 SET PRO=%1
-IF %PRO%==pro (
+IF /I "%PRO%"=="pro" (
     echo INSTALLING PRO
     SET REQUIREMENTS_FILE=minimal_requirements_pro.txt
 ) ELSE (
@@ -9,6 +9,7 @@ IF %PRO%==pro (
 
 python -m venv temp_venv
 CALL temp_venv/Scripts/activate
+CALL pip install pyinstaller
 CALL pip install -r %REQUIREMENTS_FILE%
 
 CALL pyinstaller --exclude-module=torch --name=QuranCounter --onedir --windowed --add-data "data;data" --add-data "embeddings/topics_embeddings.pkl;embeddings" --add-data "gui/resources;gui/resources" --add-data "translations;translations" --add-data "fonts;fonts" --add-data "surah_index.yml;." main.py
