8341aea411b5e4a2346d5403f8175905463e3af4
Alaa <masalha.alaa@gmail.com>
2025-03-14 23:05:05 +0200

    Added missing translation files

---
+++ translations/version_update_dialog_ar.ts
@@ -0,0 +1,17 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="ar_EG">
+<context>
+    <name>VersionUpdateDialog</name>
+    <message>
+        <location filename="../gui/version_update_dialog/version_update_dialog.ui" line="26"/>
+        <source>تحديث</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../gui/version_update_dialog/version_update_dialog.ui" line="100"/>
+        <source>تحميل</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+</TS>
---
+++ translations/version_update_dialog_en.ts
@@ -0,0 +1,17 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="ar_EG">
+<context>
+    <name>VersionUpdateDialog</name>
+    <message>
+        <location filename="../gui/version_update_dialog/version_update_dialog.ui" line="26"/>
+        <source>تحديث</source>
+        <translation>Update</translation>
+    </message>
+    <message>
+        <location filename="../gui/version_update_dialog/version_update_dialog.ui" line="100"/>
+        <source>تحميل</source>
+        <translation>Download</translation>
+    </message>
+</context>
+</TS>
